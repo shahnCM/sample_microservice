812270203649fe0c8a0bce8424f7ffd400fe3513
shahnCM <mahir.fine@hotmail.com>
2024-07-25 16:11:02 +0600

    email is set unique

--- BE/auth_ms/codes/pkg/model/user.go
+++ BE/auth_ms/codes/pkg/model/user.go
@@ -9,7 +9,7 @@ type User struct {
 	Id                  uint     `gorm:"primaryKey;autoIncrement"`
 	Username            string   `gorm:"unique;not null"`
 	Password            string   `gorm:"not null"`
-	Email               string   `gorm:"not null"`
+	Email               string   `gorm:"unique;not null"`
 	Role                string   `gorm:"type:enum('standard', 'admin');not null"`
 	SessionTokenTraceId *string  `gorm:"unique"`
 	LastSessionId       *uint    `gorm:"unique"`
