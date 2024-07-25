78c68505791e001a8b26ac4754e91ba15ba30100
shahnCM <mahir.fine@hotmail.com>
2024-07-25 17:52:18 +0600

    jwt configuration update

--- BE/auth_ms/codes/pkg/config/jwt.go
+++ BE/auth_ms/codes/pkg/config/jwt.go
@@ -6,6 +6,7 @@ import (
 
 type JwtConfig struct {
 	JwtSecret        string
+	RefreshSecret    string
 	JwtExpiresIn     string
 	RefreshExpiresIn string
 }
@@ -13,6 +14,7 @@ type JwtConfig struct {
 func GetJwtConfig() *JwtConfig {
 	return &JwtConfig{
 		JwtSecret:        os.Getenv("JWT_SECRET"),
+		RefreshSecret:    os.Getenv("REFRESH_SECRET"),
 		JwtExpiresIn:     os.Getenv("JWT_EXPIRES_IN"),
 		RefreshExpiresIn: os.Getenv("REFRESH_EXPIRES_IN"),
 	}
