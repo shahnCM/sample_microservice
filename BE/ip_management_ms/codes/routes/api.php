908eda139948688d2d847f54cc18183a648be327
shahnCM <mahir.fine@hotmail.com>
2024-07-30 03:00:21 +0600

    Added doc for ip_management service

--- BE/ip_management_ms/codes/routes/api.php
+++ BE/ip_management_ms/codes/routes/api.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\IpAddressController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'ApiUserJwtAuth'], function () {
-    Route::apiResource('ip_addresses', IpAddressController::class);
-    Route::get('action-logs', [IpAddressController::class, 'actionLogs']);
+    Route::apiResource('v1/ip_addresses', IpAddressController::class);
+    Route::get('v1/action-logs', [IpAddressController::class, 'actionLogs']);
 });
